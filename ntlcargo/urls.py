39439563371cf8dfc2edb01c1a973ff852bc461f
Yokeshwaran K <90914797+Yokeshwaran-k@users.noreply.github.com>
2024-06-04 20:17:58 +0530

    Delete ntlcargo directory

--- ntlcargo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("",views.home,name="home"),
-    path("about-us",views.about,name="about-us"),
-    path("contact",views.about,name="contact-us"),
-]
