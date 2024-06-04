e2be73649b9557185831d0b4981987e2c60c64a8
Yokeshwaran K <90914797+Yokeshwaran-k@users.noreply.github.com>
2024-06-04 20:20:52 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,28 @@
+
+    window.onscroll = function() {myFunction()};
+    
+    var header = document.getElementById("header");
+    var sticky = header.offsetTop;
+    
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+
+    var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+
+    $(document).ready(function(){
+      $('.inner-text').hide().fadeIn(2000);
+    });
+    
